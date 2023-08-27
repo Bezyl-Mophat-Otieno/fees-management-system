a0484ee492a043d9e893c2e0f7c93bacd75e48fa
BEZYL MOPHAT OTIENO <96384227+Bezyl-Mophat-Otieno@users.noreply.github.com>
2023-08-27 18:33:41 +0300

    Added junit for test reporting

--- jest.config.mjs
+++ jest.config.mjs
@@ -154,15 +154,10 @@ const config = {
   // testLocationInResults: false,
 
   // The glob patterns Jest uses to detect test files
-  testMatch: [
-    "**/__tests__/**/*.[jt]s?(x)",
-    "**/?(*.)+(spec|test).[tj]s?(x)"
-  ],
+  testMatch: ["**/__tests__/**/*.[jt]s?(x)", "**/?(*.)+(spec|test).[tj]s?(x)"],
 
   // An array of regexp pattern strings that are matched against all test paths, matched tests are skipped
-  testPathIgnorePatterns: [
-    "/node_modules/"
-  ],
+  testPathIgnorePatterns: ["/node_modules/"],
 
   // The regexp pattern or array of patterns that Jest uses to detect test files
   // testRegex: [],
@@ -193,6 +188,10 @@ const config = {
 
   // Whether to use watchman for file crawling
   // watchman: true,
+
+  jest: {
+    reporters: ["default", "jest-junit"],
+  },
 };
 
-export default  config;
+export default config;
